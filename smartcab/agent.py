9a7a361ecb0c4d9e1483c9b785a389714b483811
Ahmed Elgabaly <ahmedelgabaly@Ahmeds-MacBook-Pro.local>
2017-03-27 23:32:21 -0700

    Question 6

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -200,7 +200,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, display=False, log_metrics=True, update_delay=0.01)
+    sim = Simulator(env, display=True, log_metrics=True, update_delay=0.01, optimized=True)
     
     ##############
     # Run the simulator
